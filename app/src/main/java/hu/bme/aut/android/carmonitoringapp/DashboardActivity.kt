78c6a332523f283bdcba3ee901c6ffca06f137bd
Marton Valki <loboncosfred@gmail.com>
2019-02-17 23:22:49 +0100

    Initial project created
    some activities, layouts and navigation

---
+++ app/src/main/java/hu/bme/aut/android/carmonitoringapp/DashboardActivity.kt
@@ -0,0 +1,19 @@
+package hu.bme.aut.android.carmonitoringapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
---
+++ app/src/main/java/hu/bme/aut/android/carmonitoringapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package hu.bme.aut.android.carmonitoringapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val monitoringButton: Button = findViewById(R.id.monitoring_button)
+        monitoringButton.setOnClickListener {
+            startActivity(Intent(this, RecordLapActivity::class.java))
+        }
+
+        val dashboardButton: Button = findViewById(R.id.dashboard_button)
+        dashboardButton.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+
+        val resultsButton: Button = findViewById(R.id.results_button)
+        resultsButton.setOnClickListener { Toast.makeText(this, "Not implemented yet", Toast.LENGTH_SHORT).show() }
+
+    }
+}
